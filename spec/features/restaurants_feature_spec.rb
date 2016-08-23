1ac2a3ac5eccc870a8707d5282409158881bb165
Samed Yalniz <s.yalniz@hotmail.de>
2016-08-23 15:51:56 +0200

    update restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -12,7 +12,7 @@ feature 'restaurants' do
 
   context 'restaurants have been added'do
     before do
-      Restaurant.create(name: 'KFC')
+      Restaurant.create(name: 'KFC',description: 'Fast Food')
     end
 
     scenario 'display restaurants'do
@@ -29,21 +29,36 @@ feature 'restaurants' do
       click_link 'Add a restaurant'
       expect(current_path).to eq '/restaurants/new'
       fill_in 'Name', with: 'KFC'
+      fill_in 'Description', with: 'Fast Food'
       click_button 'Create Restaurant'
       expect(page).to have_content("KFC")
       expect(current_path).to eq '/restaurants'
+
     end
   end
 
   context 'viewing restaurants' do
-    let!(:kfc){ Restaurant.create(name: 'KFC') }
-
+    let!(:kfc){ Restaurant.create(name: 'KFC', description: "Fast Food") }
     scenario 'lets a user view a restaurant' do
     visit '/restaurants'
     click_link 'KFC'
     expect(page).to have_content 'KFC'
+    expect(page).to have_content 'Fast Food'
     expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
+  end
+
+  context 'editing restaurants' do
+      before {Restaurant.create name: 'KFC', description: 'deep fried goodness'}
+      scenario 'let a user edit a restaurant' do
+        visit '/restaurants'
+        click_link 'Edit KFC'
+        fill_in 'Name', with: 'Kentucky Fried Chicken'
+        fill_in 'Description', with: "deep fried goodness"
+        click_button 'Update Restaurant'
+        expect(page).to have_content 'Kentucky Fried Chicken'
+        expect(current_path).to eq '/restaurants'
+      end
   end
 
 
