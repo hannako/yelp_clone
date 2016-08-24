740c911149c3cfc2f1b81b5039146f96e98c5849
Giancarlo <gfabrizi@gmail.com>
2016-08-24 12:29:14 +0200

    section13:testing validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160823172427) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +27,6 @@ ActiveRecord::Schema.define(version: 20160823172427) do
   create_table "reviews", force: :cascade do |t|
     t.text     "thoughts"
     t.integer  "rating"
-
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "restaurant_id"
@@ -38,5 +35,4 @@ ActiveRecord::Schema.define(version: 20160823172427) do
   add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
 
   add_foreign_key "reviews", "restaurants"
-
 end
